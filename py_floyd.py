d7c659910bfcbc70e4669449383afc1e4f5c7fe4
ptakpiotr <piter020@outlook.com>
2024-04-29 16:37:24 +0200

    Add python sequential solution for tests

---
+++ py_floyd.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+
+def read_matrix_from_file(filepath="./matrix.txt"):
+    with open(filepath, "r") as f:
+        all_lines = [list(map(int, a.replace('\n', '').split(',')))
+                     for a in f.readlines()]
+        return all_lines
+    
+def matrix_to_edge_list(matrix):
+    edge_list = []
+    num_rows, num_cols = matrix.shape 
+
+    for i in range(num_rows):
+        for j in range(num_cols):
+            weight = matrix[i][j]
+            if weight != 0:
+                edge_list.append((i, j, weight))
+
+    return edge_list
+
+def construct_path(prev, u,v):
+    if prev[u, v] == -1:
+        return []
+    path = [v]
+    while u != v:
+        v = prev[u,v]
+        path = [v, *path]
+    return path
+
+if __name__ == "__main__":
+    matrix = np.array(read_matrix_from_file())
+    A = np.copy(matrix)
+    edge_list = matrix_to_edge_list(matrix)
+    prev = np.full_like(A, -1)
+    n = len(matrix)
+
+    for edge in edge_list:
+        prev[edge[0], edge[1]] = edge[0]
+
+    for i in range(n):
+        prev[i,i] = i + 1
+    for k in range(0, n):
+        for i in range(0, n):
+            for j in range(0, n):
+                if A[i, j] > A[i, k] + A[k, j]:
+                    A[i, j] = A[i, k] + A[k, j]
+                    prev[i, j] = prev[k, j]
+
+    print(A)
+    print(construct_path(prev, 0, 2))
+    pass
